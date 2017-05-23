3b880872a4303170f843a914120d3e57fcdc78d4
John <johnitolee@gmail.com>
2017-05-23 13:29:47 -0700

    add form page

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -7,13 +10,24 @@ app.get("/", function(req,res){
   res.render("home_page");
 });
 
-app.get("campgrounds", function(req, res){
-  var campground = [
-    {name: "Sal", image: ""},
-    {name: "gal", image: ""},
-    {name: "ran", image: ""}
+app.get("/spacegrounds", function(req, res){
+  var spacegrounds = [
+    {name: "Sal", image: "http://www.photosforclass.com/download/5518990239"},
+    {name: "gal", image: "http://www.photosforclass.com/download/5518991291"},
+    {name: "rainbow", image: "http://www.photosforclass.com/download/5519581506"}
   ];
+
+  res.render("spacegrounds_page", {spacegrounds: spacegrounds});
 });
+
+app.get("/spacegrounds/new", function(req,res){
+  res.render("new");
+});
+
+app.post("/spacegrounds", function(req, res){
+
+});
+
 
 app.listen("3000", function(){
   console.log("Server has started");
