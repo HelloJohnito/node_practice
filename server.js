e8d2ed4791efc8d0638dbc2dc8afb5fa8d6ea3bc
John <johnitolee@gmail.com>
2017-05-27 19:37:09 -0700

    add authentication:

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    Spaceground = require("./models/spaceground"),
-    Comment     = require("./models/comment"),
-    seedDB      = require("./seeds");
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose"),
+    passport      = require("passport"),
+    LocalStrategy = require("passport-local"),
+    Spaceground   = require("./models/spaceground"),
+    User          = require("./models/user"),
+    Comment       = require("./models/comment"),
+    seedDB        = require("./seeds");
 
 
 //APP CONFIG
@@ -14,9 +17,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public")); //for css
 app.set("view engine", "ejs");
 
-
 //seed data
 seedDB();
+
+//PASSPORT CONFIG
+app.use(require("express-session")({
+  secret: "Sentence used to encode and decode.",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
 
 //ROUTES
 
@@ -80,7 +100,7 @@ app.post("/spacegrounds", function(req, res){
 //////////////////////////////////////////////////////
 // Comments Route
 
-app.get("/spacegrounds/:id/comments/new", function(req, res){
+app.get("/spacegrounds/:id/comments/new", isLoggedIn, function(req, res){
   Spaceground.findById(req.params.id, function(err, spaceground){
     if(err){
       console.log(err);
@@ -90,7 +110,7 @@ app.get("/spacegrounds/:id/comments/new", function(req, res){
   });
 });
 
-app.post("/spacegrounds/:id/comments", function(req, res){
+app.post("/spacegrounds/:id/comments", isLoggedIn, function(req, res){
   Spaceground.findById(req.params.id, function(err, spaceground){
     if(err){
       console.log(err);
@@ -108,6 +128,57 @@ app.post("/spacegrounds/:id/comments", function(req, res){
     }
   });
 });
+
+//=======================
+// AUTH Routes
+//======================
+
+// show register form
+app.get("/register", function(req, res){
+  res.render("auth/register");
+});
+
+app.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("auth/register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/spacegrounds");
+    });
+  });
+});
+
+//show login
+app.get("/login", function(req, res){
+  res.render("auth/login");
+});
+
+app.post("/login", passport.authenticate("local",
+  {
+    successRedirect: "/spacegrounds",
+    failureRedirect: "/login"
+  }), function(req, res){
+});
+
+//logout
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/spacegrounds");
+});
+
+
+//middleware
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
+
+
 
 app.listen("3000", function(){
   console.log("Server has started");
